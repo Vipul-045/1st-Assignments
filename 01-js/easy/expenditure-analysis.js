b799374dfbd6526d62a4cbf23387c3d0d831dd06
Vipul-045 <vipulyadav0479@gmail.com>
2023-12-11 20:15:09 +0530

    Expenditure-analysis Solved

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,48 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let categoryTotals = [];
+  transactions.forEach(transaction => {
+    const { category , price} = transaction;
+
+    if(categoryTotals[category] === undefined ){
+      categoryTotals[category] = 0;
+    }
+
+    categoryTotals[category] += price;    
+  });
+  
+  const result = [];
+
+  for(const category in categoryTotals){
+    if(categoryTotals.hasOwnProperty(category)){
+      result.push({
+        category: category, 
+        totalSpent: categoryTotals[category],
+      })
+    }
+  }
+  return result;
 }
+
+const transactions = [
+  {
+    id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	}
+  ,
+  {
+    id: 2,
+		timestamp: 1656076900000,
+		price: 20,
+		category: 'Cloths',
+		itemName: 'Shirt',
+	}
+]
+  const result = calculateTotalSpentByCategory(transactions) ;
+  console.log(result);
 
 module.exports = calculateTotalSpentByCategory;

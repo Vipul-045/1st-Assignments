4533a14a8906f294c9dd116f6e556443a90cc423
Vipul-045 <vipulyadav0479@gmail.com>
2023-12-11 10:04:23 +0530

    Palindrom solved

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,20 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  var re = /[^A-Za-z0-9]/g;
+  let str1 = str.toString().toLowerCase().replace(re, '');
+  let a = str1.length;
+  for(let i=0 ; i<= a/2 ; i++){
+    if(str1[i] == str1[a-i-1]){
+      return true;
+    }
+    else{
+      return false;
+    }
+  } 
 }
+
+let string = "Madam";
+console.log(isPalindrome(string));
 
 module.exports = isPalindrome;
